b67e5a116c4265ee0de1c7ada48ae07c5899f572
Dan Kempster <dan.kempster@sportsbikeshop.co.uk>
2020-09-24 01:31:51 +0100

    Ensure 'kernel.root_dir' is set in testapp

--- testapp/app/AppKernel.php
+++ testapp/app/AppKernel.php
@@ -27,4 +27,20 @@ class AppKernel extends Kernel
     {
         return realpath(__DIR__ . '/../');
     }
+
+    /**
+     * Returns the kernel parameters.
+     *
+     * @return array An array of kernel parameters
+     */
+    protected function getKernelParameters()
+    {
+        $params = parent::getKernelParameters();
+
+        if ( ! isset($params['kernel.root_dir'])) {
+            $params['kernel.root_dir'] = $params['kernel.project_dir'] . DIRECTORY_SEPARATOR . 'app';
+        }
+
+        return $params;
+    }
 }
